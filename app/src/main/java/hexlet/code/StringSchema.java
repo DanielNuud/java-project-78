03eaf51ab445b4761357f5bb5983093d1aa16c66
DanielNuud <daaniel.nuud@gmail.com>
2023-12-05 15:09:14 +0200

    Update StringSchema.java

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,47 +1,27 @@
 package hexlet.code;
 
-public class StringSchema {
-    private boolean required = false;
-    private Integer minLength;
-    private String contains;
-
+public final class StringSchema extends BaseSchema {
     public StringSchema required() {
-        required = true;
+        addCheck(
+                "required",
+                value -> value != null && (value instanceof String && !((String) value).isEmpty())
+        );
         return this;
     }
 
     public StringSchema minLength(int length) {
-        minLength = length;
+        addCheck(
+                "minLength",
+                value -> value == null || ((String) value).length() >= length
+        );
         return this;
     }
 
     public StringSchema contains(String substring) {
-        contains = substring;
+        addCheck(
+                "contains",
+                value -> value == null || ((String) value).contains(substring)
+        );
         return this;
-    }
-
-    public boolean isValid(Object data) {
-        if (data == null) {
-            return !required;
-        }
-
-        if (data instanceof String) {
-            String stringValue = (String) data;
-            if (required && stringValue.isEmpty()) {
-                return false;
-            }
-
-            if (minLength != null && stringValue.length() < minLength) {
-                return false;
-            }
-
-            if (contains != null && !stringValue.contains(contains)) {
-                return false;
-            }
-
-            return true;
-        }
-
-        return false;
     }
 }
