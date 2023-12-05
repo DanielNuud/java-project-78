cdecd0fde8d89464964957c58ae9ff6e34accd31
DanielNuud <daaniel.nuud@gmail.com>
2023-12-05 15:08:49 +0200

    Create BaseSchema.java

---
+++ app/src/main/java/hexlet/code/BaseSchema.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public abstract class BaseSchema {
+    protected Map<String, Predicate<Object>> checks = new LinkedHashMap<>();
+    protected boolean required = false;
+
+    protected final void addCheck(String name, Predicate<Object> validate) {
+        checks.put(name, validate);
+    }
+
+    public final boolean isValid(Object value) {
+        if (required) {
+            Predicate<Object> requiredCheck = checks.get("required");
+            if (requiredCheck != null && !requiredCheck.test(value)) {
+                return true;
+            }
+        }
+
+        for (Predicate<Object> validate : checks.values()) {
+            if (!validate.test(value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
