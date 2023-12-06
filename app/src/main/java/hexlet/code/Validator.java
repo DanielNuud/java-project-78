185303f5d55f400d6f0d9ae9a6f179fbd7d27f19
DanielNuud <daaniel.nuud@gmail.com>
2023-12-06 15:24:11 +0200

    Update Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,19 +1,15 @@
 package hexlet.code;
 
-<<<<<<< HEAD
-=======
-import hexlet.code.StringSchema;
-
->>>>>>> a5382fd (add Validator)
 public class Validator {
     public StringSchema string() {
         return new StringSchema();
     }
-<<<<<<< HEAD
 
     public NumberSchema number() {
         return new NumberSchema();
     }
-=======
->>>>>>> a5382fd (add Validator)
+
+    public MapSchema map() {
+        return new MapSchema();
+    }
 }
