e29f7ec674ed3923cc05d56a31740cba3b6180e7
DanielNuud <daaniel.nuud@gmail.com>
2023-12-06 15:24:30 +0200

    Create MapSchema

--- app/src/main/java/hexlet/code/MapSchema.java
+++ app/src/main/java/hexlet/code/MapSchema.java
@@ -1,5 +1,47 @@
 package hexlet.code;
 
-public class MapSchema {
+import java.util.Map;
 
+public final class MapSchema extends BaseSchema {
+    public MapSchema required() {
+        addCheck(
+                "required",
+                value -> value instanceof Map && !((Map<?, ?>) value).isEmpty() || value != null
+        );
+        return this;
+    }
+
+
+    public MapSchema sizeof(int size) {
+        addCheck(
+                "sizeof",
+                value -> value == null || ((Map<?, ?>) value).size() == size
+        );
+        return this;
+    }
+
+    public MapSchema shape(Map<String, BaseSchema> schemas) {
+        addCheck(
+                "shape",
+                value -> {
+                    if (value == null || !(value instanceof Map)) {
+                        return false;
+                    }
+
+                    Map<?, ?> map = (Map<?, ?>) value;
+
+                    for (Map.Entry<String, BaseSchema> entry : schemas.entrySet()) {
+                        String key = entry.getKey();
+                        BaseSchema schema = entry.getValue();
+
+                        if (!map.containsKey(key) || !schema.isValid(map.get(key))) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+        );
+        return this;
+    }
 }
