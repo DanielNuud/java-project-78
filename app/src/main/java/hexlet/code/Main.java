7607e9cf8d7f39da8cf9bff67544d1d08cc7bfe0
DanielNuud <daaniel.nuud@gmail.com>
2023-12-08 14:55:35 +0200

    Changing permission of gradlew

---
+++ app/src/main/java/hexlet/code/Main.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+public class Main {
+    public static void main(String[] args) {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+
+        System.out.println(schema.isValid(null));
+        schema.required();
+
+        System.out.println(schema.isValid(null));
+
+
+    }
+}
