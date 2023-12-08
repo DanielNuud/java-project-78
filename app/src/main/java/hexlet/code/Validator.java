f7da464c4e934d22c9bdcfffac66b23853d7d868
DanielNuud <daaniel.nuud@gmail.com>
2023-12-08 15:36:45 +0200

    Update Validator.java

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -5,14 +5,28 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
+    /**
+     * Validates a string.
+     *
+     * @return a StringSchema for additional validation.
+     */
     public StringSchema string() {
         return new StringSchema();
     }
-
+    /**
+     * Validates a number.
+     *
+     * @return a NumberSchema for additional validation.
+     */
     public NumberSchema number() {
         return new NumberSchema();
     }
 
+    /**
+     * Validates a map.
+     *
+     * @return a MapSchema for additional validation.
+     */
     public MapSchema map() {
         return new MapSchema();
     }
