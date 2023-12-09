96706df3b0ee4c0e7adb41da11cdb821c116043e
DanielNuud <daaniel.nuud@gmail.com>
2023-12-09 12:09:07 +0200

    Update MapSchema.java

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -6,16 +6,15 @@ public final class MapSchema extends BaseSchema {
     public MapSchema required() {
         addCheck(
                 "required",
-                value -> value instanceof Map && !((Map<?, ?>) value).isEmpty() || value != null
+                value -> value instanceof Map && !((Map) value).isEmpty() || value != null
         );
         return this;
     }
 
-
     public MapSchema sizeof(int size) {
         addCheck(
                 "sizeof",
-                value -> value == null || ((Map<?, ?>) value).size() == size
+                value -> value == null || ((Map) value).size() == size
         );
         return this;
     }
@@ -23,24 +22,9 @@ public final class MapSchema extends BaseSchema {
     public MapSchema shape(Map<String, BaseSchema> schemas) {
         addCheck(
                 "shape",
-                value -> {
-                    if (value == null || !(value instanceof Map)) {
-                        return false;
-                    }
-
-                    Map<?, ?> map = (Map<?, ?>) value;
-
-                    for (Map.Entry<String, BaseSchema> entry : schemas.entrySet()) {
-                        String key = entry.getKey();
-                        BaseSchema schema = entry.getValue();
-
-                        if (!map.containsKey(key) || !schema.isValid(map.get(key))) {
-                            return false;
-                        }
-                    }
-
-                    return true;
-                }
+                value -> value instanceof Map &&
+                        schemas.entrySet().stream()
+                                .allMatch(e -> e.getValue().isValid(((Map) value).get(e.getKey())))
         );
         return this;
     }
